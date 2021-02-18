993b37afe2dd44e0f98cd5b4d97b37938bf27305
Robert Blake <robertblake@archradio.localdomain>
2021-02-17 21:13:54 -0500

    Remove leftover stuff from template.

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import searchReducer from '../features/searchForm/searchSlice';
 
 export default configureStore({
